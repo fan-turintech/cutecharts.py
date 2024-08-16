2e47181b7294d48a735f0ee20937a2f7cebf1c62
Fan Wu <fan@turintech.ai>
2024-08-16 12:36:56 +0100

    refactoring

--- cutecharts/render/engine.py
+++ cutecharts/render/engine.py
@@ -89,24 +89,13 @@ class Javascript:
         lib: Optional[Union[str, Sequence]] = None,
         css: Optional[Union[str, Sequence]] = None,
     ):
-        if isinstance(lib, str):
-            lib = [lib]
-        elif lib is None:
-            lib = []
-        if isinstance(css, str):
-            css = [css]
-        elif css is None:
-            css = []
-        self.lib = lib
-        self.css = css
+        self.lib = [lib] if isinstance(lib, str) else (lib or [])
+        self.css = [css] if isinstance(css, str) else (css or [])
         self.data = data or ""
 
     def _repr_javascript_(self):
-        r = ""
-        for c in self.css:
-            r += _css_t % c
-        for l in self.lib:
-            r += _lib_t1 % l
+        r = ''.join([_css_t % c for c in self.css])
+        r += ''.join([_lib_t1 % l for l in self.lib])
         r += self.data
         r += _lib_t2 * len(self.lib)
         return r
@@ -126,7 +115,7 @@ class RenderEngine:
             self.before_render()
 
         html = tmpl.render(chart=self)
-        with open(dest, "w+", encoding="utf8") as f:
+        with open(dest, "w", encoding="utf8") as f:
             f.write(html)
 
         return html
@@ -139,11 +128,7 @@ class RenderEngine:
             NotebookTemplateType.get(template_type).get(CurrentConfig.NOTEBOOK_TYPE)
         )
 
-        if CurrentConfig.NOTEBOOK_TYPE == NotebookType.JUPYTER_NOTEBOOK:
-            return HTML(tmpl.render(chart=self))
-
-        if CurrentConfig.NOTEBOOK_TYPE == NotebookType.JUPYTER_LAB:
-            return HTML(tmpl.render(chart=self))
+        return HTML(tmpl.render(chart=self))
 
     def _produce_assets_cfg(self):
         local_cfg, notebook_cfg = [], []
@@ -156,8 +141,6 @@ class RenderEngine:
         return local_cfg, notebook_cfg
 
     def load_javascript(self):
-        scripts = []
-        for dep in self.assets_deps:
-            value = CurrentConfig.ASSETS_DEPS_MAP.get(dep)
-            scripts.append("{}{}.js".format(self.assets_host, value))
+        scripts = [f"{self.assets_host}{CurrentConfig.ASSETS_DEPS_MAP.get(dep)}.js"
+                   for dep in self.assets_deps]
         return Javascript(lib=scripts)
